f90a8a96c4f7900edce649e7903558712faefa74
Sarada Symonds <ssymonds@broadinstitute.org>
2020-03-20 14:49:29 -0400

    python-apt issues

--- debian9/harden-images.sh
+++ debian9/harden-images.sh
@@ -59,7 +59,7 @@ install_ansible_roles () {
 run_playbook () {
 
     echo "Running playbook..."
-    ansible-playbook deb9-cis-playbook.yml
+    ansible-playbook -e ansible_python_interpreter=/usr/bin/python3 deb9-cis-playbook.yml
 
 }
 
